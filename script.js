3bb1338dab675073a8f4cc78ba522b8ca3e4f037
Ke199 pro <107533003+kpro199procreator@users.noreply.github.com>
2024-11-05 00:21:47 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 // Configuración de MathJax
 MathJax = {
+    loader: {
+        load: ['[tex]/amsmath', '[tex]/amsfonts', '[tex]/color'], // Carga de paquetes adicionales
+    },
     tex: {
-        inlineMath: [['$', '$'], ['\', '\'], ['\\{', '\\}']], // Delimitadores para inline
-        displayMath: [['$$', '$$'], ['\', '\'], ['\\{', '\\}']] // Delimitadores para display
+        inlineMath: [['$', '$'], ['\', '\'], ['\\{', '\\}']], // Delimitadores para matemáticas en línea
+        displayMath: [['$$', '$$'], ['\', '\'], ['\\{', '\\}']], // Delimitadores para matemáticas de bloque
+        packages: {'[+]': ['amsmath', 'amsfonts', 'color']}, // Incluir paquetes de matemáticas
     },
     svg: {
-        fontCache: 'global'
+        fontCache: 'global', // Cache de fuentes global
+        linebreaks: {automatic: true}, // Permite saltos de línea automáticos en SVG
+    },
+    options: {
+        renderActions: {
+            addMenu: [10, '', ''], // Opciones para menú, desactivado
+            findAnchor: [20, '', ''], // Opciones para encontrar anclas, desactivado
+        }
+    },
+    // Sección de estilos para personalizar el aspecto
+    styles: {
+        '.MathJax': {
+            'font-size': '1.2em', // Ajustar el tamaño de fuente de las matemáticas
+            'line-height': '1.2em', // Ajustar el interlineado de las matemáticas
+        },
+        '.MathJax_SVG': {
+            'background': 'none', // Sin fondo en SVG
+        },
     }
 };
 
