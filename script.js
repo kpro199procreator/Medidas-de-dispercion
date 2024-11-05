68260615a2c418b978d4214b46c89d1066c11ce9
Sucuboy_core <107533003+kpro199procreator@users.noreply.github.com>
2024-11-05 12:30:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -36,3 +36,23 @@ function redirect() {
             // Cambia la URL a la que deseas redirigir
             window.location.href = "fecuencias.html"; // Reemplaza con tu URL
         }
+
+ function toggleDarkMode() {
+            document.body.classList.toggle('dark');
+        }
+
+        function validateLogin(event) {
+            event.preventDefault();
+            const username = document.getElementById('username').value;
+            const password = document.getElementById('password').value;
+
+            // Verificar las credenciales (puedes personalizar esto)
+            if (username === 'valdo' && password === 'vv') {
+                // Mostrar las secciones y ocultar el formulario
+                document.getElementById('loginSection').style.display = 'none';
+                document.getElementById('dataSection').style.display = 'block';
+                document.getElementById('analysisSection').style.display = 'block';
+            } else {
+                alert('Credenciales incorrectas. Intenta de nuevo.');
+            }
+        }
