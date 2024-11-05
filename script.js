3457fe8bf1aeb3b2fa46f07e46c0b9d5266746f0
Ke199 pro <107533003+kpro199procreator@users.noreply.github.com>
2024-11-05 02:02:07 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -29,20 +29,4 @@ MathJax = {
     }
 };
 
-// Inicializa MathJax al cargar la ventana
-window.onload = function () {
-    MathJax.typeset();
-};
-
-// Selecciona el objeto que se moverá
-const obj = document.getElementById("moving-object");
-
-// Función para mover el objeto a una posición aleatoria
-function moveObject() {
-    const x = Math.random() * (window.innerWidth - 100); // 100 es el ancho del objeto
-    const y = Math.random() * (window.innerHeight - 100); // 100 es la altura del objeto
-    obj.style.transform = `translate(${x}px, ${y}px)`; // Aplica la transformación
-}
-
-// Mueve el objeto cada 0.5 segundos
-setInterval(moveObject, 500);
+// Inicializa Math
